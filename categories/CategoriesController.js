fdc8f0715c688e6b33074a51c9cfb4b8ac28d836
giulianagabrielli <glngabrielli@gmail.com>
2020-06-24 15:05:07 -0300

    delete e update de Categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -9,6 +9,7 @@ router.get('/admin/categories/new', (req, res)=>{
     res.render('admin/categories/new')
 })
 
+//mostrando as categorias
 router.get('/admin/categories', (req, res)=>{
     Category.findAll().then(categories=>{
         res.render('admin/categories/index', {categories: categories})
@@ -16,7 +17,7 @@ router.get('/admin/categories', (req, res)=>{
 
 })
 
-//salvando dados do formulário
+//adicionando dados do formulário
 router.post('/categories/save', (req, res)=>{
     var title = req.body.title
     if(title != undefined){
@@ -24,11 +25,63 @@ router.post('/categories/save', (req, res)=>{
             title: title,
             slug: slugify(title)
         }).then(()=>{
-            res.redirect('/')
+            res.redirect('/admin/categories')
         })
     } else {
         res.redirect('/admin/categories/new')
     }
+})
+
+//deletando categoria
+router.post('/categories/delete', (req, res)=>{
+    var id = req.body.id
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(()=>{
+                res.redirect('/admin/categories')
+            })
+
+        } else {
+            res.redirect('/admin/categories')
+        }
+    } else {
+        res.redirect('/admin/categories')
+    }
+})
+
+//editar categoria
+router.get('/admin/categories/edit/:id', (req, res)=>{
+    var id = req.params.id
+    if(isNaN(id)){
+        res.redirect('/admin/categories')
+    }
+    Category.findByPk(id).then(category=>{
+        if(category != undefined){
+            res.render('admin/categories/edit', {category: category})
+        } else {
+            res.redirect('/admin/categories')
+        }
+    }).catch(error=>{
+        res.redirect('/admin/categories')
+    })
+})
+
+//salvando alteração no db
+router.post('/categories/update', (req, res)=>{
+    var id = req.body.id 
+    var title = req.body.title 
+
+    Category.update({title: title, slug:slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(()=>{
+        res.redirect('/admin/categories')
+    })
 })
 
 
