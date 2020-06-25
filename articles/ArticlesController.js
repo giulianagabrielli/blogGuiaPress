63f2c94c0d60b59ad59cf6385a5e0a963500156a
giulianagabrielli <glngabrielli@gmail.com>
2020-06-25 12:20:41 -0300

    update e paginação dos artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -59,4 +59,75 @@ router.post('/articles/delete', (req, res)=>{
     }
 })
 
+//editando artigos
+router.get('/admin/articles/edit/:id', (req, res)=>{
+    var id = req.params.id
+    Article.findByPk(id).then(article=>{
+        if(article != undefined){
+            Category.findAll().then(categories=>{
+                res.render('admin/articles/edit', {categories: categories, article: article})
+            })
+        } else {
+            res.redirect('/')
+        }
+    }).catch(error=>{
+        res.redirect('/')
+    })
+})
+
+//salvando alterações no banco de dados
+router.post('/articles/update', (req, res)=>{
+    var id = req.body.id 
+    var title = req.body.title 
+    var body = req.body.body 
+    var category = req.body.category
+
+    Article.update({title: title, body: body, categoryId: category, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(()=>{
+        res.redirect('/admin/articles')
+    }).catch(error=>{
+        res.redirect('/')
+    })
+})
+
+//paginação
+router.get('/articles/page/:num', (req, res)=>{
+    var page = req.params.num 
+    var offset = 0
+
+    if(isNaN(page) || page == 1){
+        offset = 0
+    } else {
+        offset = (parseInt(page)-1) * 4 //limit
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset,
+        order:[['id', 'DESC']]
+    }).then(articles=>{
+
+        var next
+        if(offset + 4 >= articles.count){
+            next = false
+        } else {
+            next = true
+        }
+
+        var result = {
+            page: parseInt(page),
+            next: next,
+            articles: articles,
+        }
+
+        Category.findAll().then(categories=> {
+            res.render('admin/articles/page', {result: result, categories: categories})
+        })
+        
+    })
+})
+
 module.exports = router
--- index.js
+++ index.js
@@ -32,6 +32,58 @@ connection
 app.use('/', CategoriesController)
 app.use('/', ArticlesController)
 
+//rota home
+app.get('/', (req, res)=>{
+    Article.findAll({
+        order: [['id', 'DESC']],
+        limit: 4
+    }).then(articles=>{
+        Category.findAll().then(categories=>{
+            res.render('index', {articles: articles, categories: categories})
+        })
+    })
+})
+
+//procurar artigo pelo slug
+app.get('/:slug', (req, res)=>{
+    var slug = req.params.slug 
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article=>{
+        if(article != undefined){
+            Category.findAll().then(categories=>{
+                res.render('article', {article: article, categories: categories})
+            })
+        } else {
+            res.redirect('/')
+        }
+    }).catch(error=>{
+        res.redirect('/')
+    })
+})
+
+//filtro de categorias
+app.get('/category/:slug', (req, res)=>{
+    var slug = req.params.slug 
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category=>{
+        if(category != undefined){
+            Category.findAll().then(categories=>{
+                res.render('index', {articles: category.articles, categories: categories})
+            })
+        } else {
+            res.redirect('/')
+        }
+    }).catch(error=>{
+        res.redirect('/')
+    })
+})
 
 app.listen(3333, ()=>{
     console.log('Servidor iniciado na porta 3333.')
